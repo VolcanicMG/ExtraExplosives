ac7d54e906593f947aab2e6f8b9b6deeb42ce981
VolcanicMG <57544191+VolcanicMG@users.noreply.github.com>
2020-03-31 16:32:28 -0600

    Add files via upload

--- ExtraExplosives.cs
+++ ExtraExplosives.cs
@@ -1,5 +1,20 @@
 using Terraria.ModLoader;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+using Terraria.Localization;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using Microsoft.Xna.Framework.Input;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
 namespace ExtraExplosives
 {
 	public class ExtraExplosives : Mod
---
+++ Projectiles/BasicExplosiveProjectile.cs
@@ -0,0 +1,68 @@
+using Terraria.ModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+using Terraria.Localization;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using Microsoft.Xna.Framework.Input;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
+
+namespace ExtraExplosives.Projectiles
+{
+	public class BasicExplosiveProjectile : ModProjectile
+	{
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("BasicExplosiveProjectile");
+            //Tooltip.SetDefault("Your one stop shop for all your turretaria needs.");
+        }
+
+        public override void SetDefaults()
+        {
+            
+            projectile.width = 13;   //This defines the hitbox width
+            projectile.height = 19;    //This defines the hitbox height
+            projectile.aiStyle = 16;  //How the projectile works, 16 is the aistyle Used for: Grenades, Dynamite, Bombs, Sticky Bomb.
+            projectile.friendly = true; //Tells the game whether it is friendly to players/friendly npcs or not
+            projectile.penetrate = -1; //Tells the game how many enemies it can hit before being destroyed
+            projectile.timeLeft = 170; //The amount of time the projectile is alive for
+        }
+
+
+
+        public override void Kill(int timeLeft)
+        {
+
+            Vector2 position = projectile.Center;
+            Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
+            int radius = 2;     //this is the explosion radius, the highter is the value the bigger is the explosion
+
+            for (int x = -radius; x <= radius; x++)
+            {
+                for (int y = -radius; y <= radius; y++)
+                {
+                    int xPosition = (int)(x + position.X / 16.0f);
+                    int yPosition = (int)(y + position.Y / 16.0f);
+
+                    if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
+                    {
+                        WorldGen.KillTile(xPosition, yPosition, false, false, false);  //this make the explosion destroy tiles  
+                        Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);  //this is the dust that will spawn after the explosion
+                    }
+                }
+            }
+        }
+
+
+    }
+}
