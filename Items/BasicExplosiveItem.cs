b4c1d215dd5aa0c064009619d9cf5352c237fbfe
VolcanicMG <57544191+VolcanicMG@users.noreply.github.com>
2020-04-04 01:11:12 -0600

    Add files via upload

--- Items/BasicExplosiveItem.cs
+++ Items/BasicExplosiveItem.cs
@@ -31,7 +31,7 @@ namespace ExtraExplosives.Items
         public override void SetDefaults()
         {
 
-            item.damage = 0;     //The damage stat for the Weapon.                
+            item.damage = 0;     //The damage stat for the Weapon.      
             item.width = 10;    //sprite width
             item.height = 32;   //sprite height
             item.maxStack = 999;   //This defines the items max stack
@@ -40,8 +40,8 @@ namespace ExtraExplosives.Items
             item.rare = 0;     //The color the title of your item when hovering over it ingame
             item.UseSound = SoundID.Item1; //The sound played when using this item
             item.useAnimation = 20;  //How long the item is used for.
-            item.useTime = 20;     //How fast the item is used.
-            item.value = Item.buyPrice(0, 0, 3, 0);   //How much the item is worth, in copper coins, when you sell it to a merchant. It costs 1/5th of this to buy it back from them. An easy way to remember the value is platinum, gold, silver, copper or PPGGSSCC (so this item price is 3 silver)
+            //item.useTime = 20;     //How fast the item is used.
+            item.value = Item.buyPrice(0, 0, 0, 15);   //How much the item is worth, in copper coins, when you sell it to a merchant. It costs 1/5th of this to buy it back from them. An easy way to remember the value is platinum, gold, silver, copper or PPGGSSCC (so this item price is 3 silver)
             item.noUseGraphic = true;
             item.noMelee = true;      //Setting to True allows the weapon sprite to stop doing damage, so only the projectile does the damge
             item.shoot = mod.ProjectileType("BasicExplosiveProjectile"); //This defines what type of projectile this item will shoot
@@ -61,9 +61,9 @@ namespace ExtraExplosives.Items
         public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.StoneBlock, 3);
-			recipe.AddIngredient(ItemID.CopperBow, 1);
-			recipe.AddIngredient(ItemID.Wood, 3);
+			recipe.AddIngredient(ItemID.Grenade, 1);
+			recipe.AddIngredient(ItemID.Gel, 5);
+			recipe.AddIngredient(ItemID.StoneBlock, 2);
 			recipe.AddTile(TileID.WorkBenches);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
