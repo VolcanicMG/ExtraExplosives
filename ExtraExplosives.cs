4b94903416509312c4d26d59b19169878e2ba830
VolcanicMG <57544191+VolcanicMG@users.noreply.github.com>
2020-06-13 23:53:47 -0600

    V2.2

---
+++ ExtraExplosives.cs
@@ -0,0 +1,297 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using ExtraExplosives.Items.Explosives;
+using ExtraExplosives.Projectiles;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.UI;
+
+using static ExtraExplosives.GlobalMethods;
+
+namespace ExtraExplosives
+{
+	public class NewBulletBoomItem
+	{								// These are for
+		public int itemID;			// crafting recipe
+		public string projName;		// display name, tooltip, and registering with tml
+		public string displayName;
+
+		public NewBulletBoomItem(int itemID, string projName, string displayName)
+		{
+			this.itemID = itemID;
+			this.projName = projName;
+			this.displayName = displayName;
+		}
+	}
+
+	public class NewBulletBoomProjectile
+	{
+		public int projectileID;		// so the projectile can be shot
+		public string projName;			// registering with terraria and display name
+
+		public NewBulletBoomProjectile(int projectileID, string projName)
+		{
+			this.projectileID = projectileID;
+			this.projName = projName;
+		}
+	}
+	
+	public class ExtraExplosives : Mod
+	{
+		// Modded Bullet Boom Support (Not in use but necessary)
+		public static IDictionary<int, int> mapItemToItemID;
+		public static bool generateForeignBulletBooms;
+		public static void AddPair(int item, int id)
+		{
+				mapItemToItemID.Add(item, id);
+		}
+		public static void NewRegister(NewBulletBoomItem item, NewBulletBoomProjectile proj)
+		{
+			AddNewBulletItem(item);
+			AddNewBulletProj(proj);
+		}
+		private static void AddNewBulletItem(NewBulletBoomItem item)
+		{
+			_bulletBoomItems.Add(item);
+		}
+
+		private static void AddNewBulletProj(NewBulletBoomProjectile proj)
+		{
+			_bulletBoomProjectiles.Add(proj);
+		}
+		// This is where the info for the bulletboom generation is stored, not quite (fully) dynamic sadly
+		// Item List (Note lists are 1-1 and ordered, changing order will break loading)
+		static List<NewBulletBoomItem> _bulletBoomItems = new List<NewBulletBoomItem>() 
+		{
+			new NewBulletBoomItem(ItemID.MusketBall, "MusketBall", "Musket"), 
+			new NewBulletBoomItem(ItemID.SilverBullet, "SilverBullet", "Silver"),
+			new NewBulletBoomItem(ItemID.MeteorShot, "MeteorShot", "Meteor"),
+			new NewBulletBoomItem(ItemID.CrystalBullet, "CrystalBullet","Crystal"), 
+			new NewBulletBoomItem(ItemID.CursedBullet, "CursedBullet","Cursed"), 
+			new NewBulletBoomItem(ItemID.ChlorophyteBullet, "ChlorophyteBullet","Chlorophyte"), 
+			new NewBulletBoomItem(ItemID.HighVelocityBullet, "HighVelocityBullet","High Velocity"), 
+			new NewBulletBoomItem(ItemID.IchorBullet, "IchorBullet","Ichor"), 
+			new NewBulletBoomItem(ItemID.VenomBullet, "VenomBullet","Venom"), 
+			new NewBulletBoomItem(ItemID.PartyBullet, "PartyBullet","Party"), 
+			new NewBulletBoomItem(ItemID.NanoBullet, "NanoBullet","Nano"), 
+			new NewBulletBoomItem(ItemID.ExplodingBullet, "ExplodingBullet","Exploding"), 
+			new NewBulletBoomItem(ItemID.GoldenBullet, "GoldenBullet","Golden"), 
+			new NewBulletBoomItem(ItemID.MoonlordBullet, "LuminiteBullet","Luminite")
+		};
+		// Projectile List
+		static List<NewBulletBoomProjectile> _bulletBoomProjectiles = new List<NewBulletBoomProjectile>()
+		{
+			new NewBulletBoomProjectile(ProjectileID.Bullet, "MusketBall"), 
+			new NewBulletBoomProjectile(ProjectileID.Bullet, "SilverBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.MeteorShot, "MeteorShot"),
+			new NewBulletBoomProjectile(ProjectileID.CrystalBullet, "CrystalBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.CursedBullet, "CursedBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.ChlorophyteBullet, "ChlorophyteBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.BulletHighVelocity, "HighVelocityBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.IchorBullet, "IchorBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.VenomBullet, "VenomBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.PartyBullet, "PartyBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.NanoBullet, "NanoBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.ExplosiveBullet, "ExplodingBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.GoldenBullet, "GoldenBullet"), 
+			new NewBulletBoomProjectile(ProjectileID.MoonlordBullet, "LuminiteBullet")
+		};
+		
+		//move the first 4 over to player????
+		internal static ModHotKey TriggerExplosion;
+
+		internal static ModHotKey TriggerUIReforge;
+
+		public static bool NukeActivated;
+		public static bool NukeActive;
+		public static Vector2 NukePos;
+		public static bool NukeHit;
+
+		internal static float dustAmount;
+		internal UserInterface ExtraExplosivesUserInterface;
+		internal UserInterface ExtraExplosivesReforgeBombInterface;
+
+		public static string GithubUserName => "VolcanicMG";
+		public static string GithubProjectName => "ExtraExplosives";
+		
+		// Create the item to item id reference (used with cpt explosive) Needs to stay loaded
+		public ExtraExplosives()
+		{
+			mapItemToItemID = new Dictionary<int,int>();
+		}
+		
+		// Registers modded projectiles 
+		public void RunRegistry()
+		{
+			for (int i = 0; i < _bulletBoomItems.Count; i++)	// loop through array
+			{
+				// creates projectile and registers it with tml
+				BulletBoomProjectile projectile = new BulletBoomProjectile(_bulletBoomProjectiles[i].projectileID, _bulletBoomProjectiles[i].projName);
+				AddProjectile(_bulletBoomProjectiles[i].projName, projectile);
+				//Creates items and register it
+				BulletBoomItem item = new BulletBoomItem(_bulletBoomItems[i].displayName, projectile);
+				AddItem(_bulletBoomItems[i].projName, item);
+				// map the item to its new id and ammo to the item 
+				ExtraExplosives.AddPair(_bulletBoomItems[i].itemID, item.item.type);
+			}
+		}
+
+		public override void HandlePacket(BinaryReader reader, int whoAmI)
+		{
+			int check = reader.ReadVarInt();
+			////Don't use as of right now
+			//if (reader.ReadString() == "boom") //set to a byte,
+			//{
+			//	if (Main.netMode == NetmodeID.Server)//set the other players to have the same properties besides the client
+			//	{
+			//		ModPacket myPacket = GetPacket();
+			//		myPacket.Write("boom");
+			//		myPacket.Send(ignoreClient: whoAmI);
+			//	}
+			//	else//set what you want to happen
+			//	{
+			//		NukeActive = true;
+			//	}
+			//}
+
+			//if (reader.ReadString() == "Set")
+			//{
+			//	if (Main.netMode == NetmodeID.Server)
+			//	{
+			//		ModPacket myPacket = GetPacket();
+			//		myPacket.Write("Set");
+			//		myPacket.Send(ignoreClient: whoAmI);
+			//	}
+			//	else
+			//	{
+			//		NukeActivated = true;
+			//	}
+			//}
+
+			//Vector2 pos = reader.ReadPackedVector2();
+			//NukePos = pos;
+
+			if (check == 1) //to make sure only one player can spawn in a nuke at a time in MP
+			{
+				if (Main.netMode == NetmodeID.Server)
+				{
+					ModPacket myPacket = GetPacket();
+					myPacket.WriteVarInt(1);
+					myPacket.Send(ignoreClient: whoAmI);
+				}
+				else
+				{
+					NukeActivated = true;
+				}
+			}
+
+			if (check == 2) //sets NukeHit to false for all players
+			{
+				if (Main.netMode == NetmodeID.Server)
+				{
+					ModPacket myPacket = GetPacket();
+					myPacket.WriteVarInt(2);
+					myPacket.Send(ignoreClient: whoAmI);
+				}
+				else
+				{
+					NukeHit = false;
+				}
+			}
+		}
+
+		public override void PostSetupContent()
+		{
+			Mod censusMod = ModLoader.GetMod("Census");
+			if (censusMod != null)
+			{
+				// Here I am using Chat Tags to make my condition even more interesting.
+				// If you localize your mod, pass in a localized string instead of just English.
+				// Additional lines for additional town npc that your mod adds
+				// Simpler example:
+				censusMod.Call("TownNPCCondition", NPCType("CaptainExplosive"), "Kill King Slime"); //Change later for the boss
+			}
+
+			base.PostSetupContent();
+		}
+
+		public override void UpdateUI(GameTime gameTime)
+		{
+			ExtraExplosivesUserInterface?.Update(gameTime);
+			//ExtraExplosivesReforgeBombInterface?.Update(gameTime);
+		}
+
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+
+			int inventoryIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Inventory"));
+			if (inventoryIndex != -1)
+			{
+				layers.Insert(inventoryIndex, new LegacyGameInterfaceLayer(
+					"ExtraExplosives: UI",
+					delegate
+					{
+						// If the current UIState of the UserInterface is null, nothing will draw. We don't need to track a separate .visible value.
+						ExtraExplosivesUserInterface.Draw(Main.spriteBatch, new GameTime());
+						return true;
+					},
+					InterfaceScaleType.UI)
+				);
+				layers.Insert(inventoryIndex, new LegacyGameInterfaceLayer(
+					"ExtraExplosives: ReforgeBombUI",
+					delegate
+					{
+						// If the current UIState of the UserInterface is null, nothing will draw. We don't need to track a separate .visible value.
+						ExtraExplosivesReforgeBombInterface.Draw(Main.spriteBatch, new GameTime());
+						return true;
+					},
+					InterfaceScaleType.UI)
+				);
+			}
+		}
+
+		public override void Load()
+		{
+			Logger.InfoFormat("{0} Extra Explosives logger", Name);
+
+			ExtraExplosivesUserInterface = new UserInterface();
+			ExtraExplosivesReforgeBombInterface = new UserInterface();
+
+			TriggerExplosion = RegisterHotKey("Explode", "Mouse2");
+			TriggerUIReforge = RegisterHotKey("Open Reforge Bomb UI", "P");
+
+			if (Main.netMode != NetmodeID.Server)
+			{
+				//load in the shaders
+				Ref<Effect> screenRef = new Ref<Effect>(GetEffect("Effects/Shader")); // The path to the compiled shader file.
+				Filters.Scene["Bang"] = new Filter(new ScreenShaderData(screenRef, "Bang"), EffectPriority.VeryHigh); //float4 name
+				Filters.Scene["Bang"].Load();
+
+				Ref<Effect> screenRef2 = new Ref<Effect>(GetEffect("Effects/NukeShader")); // The path to the compiled shader file.
+				Filters.Scene["BigBang"] = new Filter(new ScreenShaderData(screenRef2, "BigBang"), EffectPriority.VeryHigh); //float4 name
+				Filters.Scene["BigBang"].Load();
+			}
+			
+			// Check config setting, then run registry
+			// If config setting is enabled, warns the user since it might cause problems when handling poorly written mods
+			if (generateForeignBulletBooms)
+			{
+				// Logger info because this feature is janky as can be
+				// Use warn on first so its stands out since it will eventually cause problems
+				Logger.Warn("You are using the dynamic bullet boom generation feature, this may result in insability while loading");
+				Logger.Info("This feature, while stable, can be problematic with both lots of mods and mods with strange naming conventions for their items\n" +
+				            "If you see this, you are probably having problems loading, disabling Extra Explosives may solve them");
+				ForeignModParsing.PostLoad();	// Run if config setting is set
+			}
+			RunRegistry();		// Always run to load standard bullet booms
+
+		}
+	}
+}
---
+++ Items/DevItem.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExtraExplosives.Items
+{
+	public class DevItem : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Projectile Tester");
+			Tooltip.SetDefault("For testing purposes");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 0;	 //The damage stat for the Weapon.
+			item.width = 20;	//sprite width
+			item.height = 20;   //sprite height
+			item.maxStack = 1;   //This defines the items max stack
+			item.consumable = false;  //Tells the game that this should be used up once fired
+			item.useStyle = 1;   //The way your item will be used, 1 is the regular sword swing for example
+			item.useStyle = 1;   //The way your item will be used, 1 is the regular sword swing for example	
+			item.rare = 11;	 //The color the title of your item when hovering over it ingame
+			item.UseSound = SoundID.Item1; //The sound played when using this item
+			item.useAnimation = 20;  //How long the item is used for.
+			item.useTime = 20;
+			item.noUseGraphic = true;
+			item.noMelee = true;	  //Setting to True allows the weapon sprite to stop doing damage, so only the projectile does the damge
+			item.noMelee = true;	  //Setting to True allows the weapon sprite to stop doing damage, so only the projectile does the damage
+			item.shoot = 386; //This defines what type of projectile this item will shoot
+			item.shootSpeed = 15f; //This defines the projectile speed when shot
+			//item.createTile = mod.TileType("ExplosiveTile");
+		}
+	}
+}
