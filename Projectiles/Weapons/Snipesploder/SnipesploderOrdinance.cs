543008464bf691478c83b57dea2d1ef0eff3f7f4
Ethan <hoff.gamedevelopment@gmail.com>
2020-09-27 21:10:47 -0600

    fall cleaning

--- Projectiles/Weapons/Snipesploder/SnipesploderOrdinance.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExtraExplosives.Projectiles.Weapons.Snipesploder
-{
-    public class SnipesploderOrdinance// : ExplosiveProjectile
-    {
-        //protected override string explodeSoundsLoc { get; }
-        //protected override string goreFileLoc { get; }
-    }
-}
